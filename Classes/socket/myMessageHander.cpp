2a7b55e80ee79e43c887957994844fd4d8965d58
hyz1992 <18086089679@163.com>
2015-12-01 21:49:59 +0800

    xxxx

--- Classes/socket/myMessageHander.cpp
+++ Classes/socket/myMessageHander.cpp
@@ -4,6 +4,7 @@
 void myMessageHander::registerListener(int actionId)
 {
 	myMessageManager::getInstance()->rigeterMessageListener(actionId, this);
+	_actionValueMap.insert(std::pair<int, int>(actionId, actionId));
 }
 
 void myMessageHander::destoryListener(int actionId)
--- Classes/socket/myMessageHander.h
+++ Classes/socket/myMessageHander.h
@@ -20,11 +20,22 @@ class myMessageHander
 public:
 	virtual void sendMessageToServer(int actionId) = 0;//根据不同的消息，发送的数据肯定也不一样
 	virtual void receiveMessageFromServer(SmartMessageData * data) = 0;
-
+	~myMessageHander()
+	{
+		std::map<int, int>::iterator itor = _actionValueMap.begin();
+		for (; itor != _actionValueMap.end(); itor++)
+		{
+			if (itor->first == itor->second )
+			{
+				destoryListener(itor->first);
+			}
+		}
+	}
 protected:
 	void registerListener(int actionId);
+private:
 	void destoryListener(int actionId);
 private:
-
+	std::map<int, int> _actionValueMap;
 	
 };
